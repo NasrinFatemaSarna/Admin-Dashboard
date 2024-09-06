2e7927411f17e267b686dd591186270a599163dc
NasrinFatemaSarna <sarnaqueen@gmail.com>
2024-09-05 19:09:07 -0700

    Fix port binding

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Ensure that the app binds to 0.0.0.0 for external access
+    port: process.env.PORT || 3000 // Use the platform-provided port or fall back to 3000 for local testing
+  }
 })
+
